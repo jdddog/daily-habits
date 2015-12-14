3f3367fb20e9471c42c32ab4dd55be8a21dfcd69
Jamie Diprose <jamie.diprose@gmail.com>
2015-12-14 14:27:05 +1300

    Update

--- app/js/main-controller.js
+++ app/js/main-controller.js
@@ -2,8 +2,8 @@ var dailyHabits = angular.module('dailyHabits', ['ngMaterial']);
 
 dailyHabits.controller('mainCtrl', function ($scope) {
     $scope.client = new UoACalendarClient("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmlnX2lhdCI6MTQyMjQ5ODk0OSwiZXhwIjoxNDIyNDk5MjQ5LCJ1c2VyX2lkIjoyLCJ1c2VybmFtZSI6ImRldmVsb3BlciIsImVtYWlsIjoidGVzdEBhdWNrbGFuZC5hYy5ueiJ9.7jLkEBovT2HvT2noL4xdIhddaY8wpZpEVYEDHnnNm1Y");
-    $scope.events = [];
     $scope.calendarId = null;
+    $scope.events = [];
 
     $scope.initialiseCalendar = function () {
         $scope.client.addCalendar("Daily Habits").then(function (data) {
