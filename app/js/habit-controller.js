e51fd88ed209977597666acbbaf8cc1118396c3e
Jamie Diprose <jamie.diprose@gmail.com>
2015-12-16 13:59:28 +1300

    Update

--- app/js/habit-controller.js
+++ app/js/habit-controller.js
@@ -1,5 +1,4 @@
-
-angular.module('dailyHabits').controller('habitCtrl', function ($scope, localStorageService, $q) {
+angular.module('dailyHabits').controller('habitCtrl', function ($scope, localStorageService) {
 
     $scope.validateDate = function (date) {
         return date > moment();
@@ -14,76 +13,57 @@ angular.module('dailyHabits').controller('habitCtrl', function ($scope, localSto
     };
 
     $scope.saveHabit = function () {
-        if ($scope.selectedHabit.hasOwnProperty('habitId')) {
-            localStorageService.set($scope.selectedHabit.habitId, $scope.selectedHabit);
-            $scope.closeDialog();
-            $scope.deleteEvents($scope.selectedHabit).then(function (data) {
-                $scope.createEvents($scope.selectedHabit).then(function (data) {
-                    $scope.today();
-                });
-            });
-        }
-        else {
+        $scope.closeDialog();
+
+        if (!$scope.selectedHabit.hasOwnProperty('habitId')) {
             $scope.selectedHabit.habitId = uuid.v4();
-            localStorageService.set($scope.selectedHabit.habitId, $scope.selectedHabit); //TODO: change to calendar api when can store custom fields in calendar
-            $scope.closeDialog();
-            $scope.createEvents($scope.selectedHabit).then(function (data) {
-                $scope.today();
-            });
+            localStorageService.set($scope.selectedHabit.habitId, $scope.selectedHabit);
+            $scope.createEvents($scope.selectedHabit);
+        }
+        else
+        {
+            localStorageService.set($scope.selectedHabit.habitId, $scope.selectedHabit);
+            $scope.deleteEvents($scope.selectedHabit);
         }
     };
 
     $scope.createEvents = function (habit) {
-        return $q(function (resolve, reject) {
-            var currentDate = moment();
-            var events = [];
+        var currentDate = moment();
+        var events = [];
 
-            for (var d = 0; d < habit.numDays; d++) {
-                var day = currentDate.day();
-
-                events.push({
-                    title: habit.name,
-                    start: currentDate.clone().toDate(),
-                    end: currentDate.clone().toDate(),
-                    habitId: habit.habitId
-                });
-
-                currentDate.add(1, 'day');
-            }
-
-            $scope.client.addEvents($scope.calendarId, events).then(function (data) {
-                resolve(data);
-            }).catch(function (error) {
-                reject(error);
+        for (var d = 0; d < habit.numDays; d++) {
+            events.push({
+                title: habit.name,
+                start: currentDate.clone().toDate(),
+                end: currentDate.clone().toDate(),
+                habitId: habit.habitId
             });
+
+            currentDate.add(1, 'day');
+        }
+
+        $scope.client.addEvents($scope.calendarId, events).then(function (data) {
+            $scope.today();
         });
     };
 
     $scope.deleteEvents = function (habit) {
-        return $q(function (resolve, reject) {
-            var currentDate = moment().startOf('day');
+        var currentDate = moment().startOf('day');
 
-            $scope.client.findEvents($scope.calendarId, currentDate.toDate(), null).then(function (data) {
-                var eventIds = [];
-                for (var i = 0; i < data.length; i++) {
-                    var event = data[i];
-                    if (habit.habitId == event.habitId && event.start >= currentDate) {
-                        eventIds.push(event.id);
-                    }
+        $scope.client.listEvents($scope.calendarId).then(function (data) {
+            var eventIds = [];
+            for (var i = 0; i < data.length; i++) {
+                var event = data[i];
+                if (habit.habitId == event.habitId && event.start >= currentDate) {
+                    eventIds.push(event.id);
                 }
+            }
 
-                $scope.client.deleteEvents($scope.calendarId, eventIds).then(function (data) {
-                    resolve(data);
-                }).catch(function (error) {
-                    resolve(error);
-                });
-
-            }).catch(function (error)
-                {
-                    console.log(JSON.stringify(error));
-                    reject(error);
-                }
-            );
+            $scope.client.deleteEvents($scope.calendarId, eventIds).then(function (data) {
+                $scope.createEvents(habit);
+            }).catch(function (error) {
+                console.log(JSON.stringify(error));
+            });
         });
     };
 });
