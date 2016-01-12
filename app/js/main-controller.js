0c1fda82e44888b69c11e5d8e356acbc26e75aa6
Jamie Diprose <jamie.diprose@gmail.com>
2016-01-12 17:52:54 +1300

    Fix class name

--- app/js/main-controller.js
+++ app/js/main-controller.js
@@ -1,8 +1,7 @@
 var dailyHabits = angular.module('dailyHabits', ['ngMaterial']);
 
 dailyHabits.controller('mainCtrl', function ($scope) {
-    $scope.client = new DiaryClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NTI1NzQ1MDgsImVtYWlsIjoiIiwidXNlcm5hbWUiOiJqZGlwMDA0QGF1Y2tsYW5kLmFjLm56IiwidXNlcl9pZCI6Mywib3JpZ19pYXQiOjE0NTI1NzQyMDh9.NYY5hvbhnzdSGez6MTKaD1Yb1VL3YS1IwXn-UkF2hGA");
-    $scope.calendarId = null;
+    $scope.client = new DiaryClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0NTI1NzQ1MDgsImVtYWlsIjoiIiwidXNlcm5hbWUiOiJqZGlwMDA0QGF1Y2tsYW5kLmFjLm56IiwidXNlcl9pZCI6Mywib3JpZ19pYXQiOjE0NTI1NzQyMDh9.NYY5hvbhnzdSGez6MTKaD1Yb1VL3YS1IwXn-UkF2hGA");    $scope.calendarId = null;
     $scope.events = [];
 
     $scope.initialiseCalendar = function () {
